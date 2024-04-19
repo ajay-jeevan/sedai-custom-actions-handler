fcd5a6d38ab4d97835841111a0c48043de06b23a
ajay-jeevan <110399645+ajay-jeevan@users.noreply.github.com>
2024-04-19 15:12:10 +0530

    FIrst commit

---
+++ FlaskApp/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, jsonify, request
+import requests
+import json
+
+# Always use relative import for custom module
+from .package.module import MODULE_VALUE
+
+app = Flask(__name__)
+
+@app.route("/performAction", methods=['POST'])
+def performAction():
+
+    workflow_auto_url = 'https://rngnbyyii56mqwsffdwlnkwawq0zxjfd.lambda-url.us-east-1.on.aws/'
+
+    response = requests.post(workflow_auto_url, json=request.json)
+
+
+    if response.status_code == 200:
+        processed_data = response.json()
+        return jsonify({'processed_data': processed_data}), 200
+    else:
+        return jsonify({'Error': 'Failed to fetch data from the workflow auto service'}), response.status_code
+
+
+
+@app.route("/statusCheck", methods=['GET'])
+def hello(name: str):
+    return f"hello {name}"
+
+if __name__ == "__main__":
+    app.run()
---
+++ README.md
@@ -0,0 +1,2 @@
+# sedai-custom-action-handler
+Serverless function code for handling custom actions API from core
