87a8e63220de25a48802e77ce39c2446efdfd51c
ajay-jeevan <110399645+ajay-jeevan@users.noreply.github.com>
2024-05-02 17:06:02 +0530

    updated code to pass host name

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -13,26 +13,29 @@ def performAction():
 
     request_json = request.json
     action_name = request_json.get('proposedAction').get('actionName')
+    host_name = request_json.get('resourceActionDetails').get('instanceDetails').get('instanceId')
 
     headers = {
         "Content-Type": "application/json",
         "St2-Api-Key": os.getenv("KEY")
     }
 
-    payload = {
-        "action": action_name,
-         "parameters": {
-        "hostvalue": "Sedai"
+    if action_name == 'REBOOT':
+
+        payload = {
+            "action": "sedaitest.sedaitest",
+            "parameters": {
+            "hostvalue": host_name
+            }
         }
-    }
 
-    response = requests.post(workflow_auto_url, json=payload, headers=headers)
+        response = requests.post(workflow_auto_url, json=payload, headers=headers)
 
-    if response.status_code == 200:
-        processed_data = response.json()
-        return jsonify({'processed_data': processed_data}), 200
-    else:
-        return jsonify({'Error': 'Failed to fetch data from the workflow auto service'}), response.status_code
+        if response.status_code == 200:
+            processed_data = response.json()
+            return jsonify({'processed_data': processed_data}), 200
+        else:
+            return jsonify({'Error': 'Failed to fetch data from the workflow auto service'}), response.status_code
 
 
 
